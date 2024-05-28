8677024e50e1be56d28decbf3b39622a3288e3f2
Eduardo-correa1 <159133054+Eduardo-correa1@users.noreply.github.com>
2024-05-28 14:29:35 +0000

    terminado

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,14 +1,15 @@
 import React, {useState} from "react";
 
 const TrafficLight = () =>{
+    const [selectedLight, setSelectedLight] = useState("secondLight");
     return(
         <div>
             <div className="card barra" style={{height: "120px", width: "15px", backgroundColor: "grey", margin: "auto"}}>
             </div>
             <div className="card semaforo" style={{height: "305px", width: "120px", backgroundColor: "grey", margin: "auto"}}>
-                <div className="firstLight"></div>
-                <div className="secondLight"></div>
-                <div className="thirdLight"></div>
+                <div className={`firstLight ${selectedLight === "firstLight" ? "sombraRoja" : ""}`} onClick={()=> setSelectedLight("firstLight")}></div>
+                <div className={`secondLight ${selectedLight === "secondLight" ? "sombraAmarilla" : ""}`} onClick={()=> setSelectedLight("secondLight")}></div>
+                <div className={`thirdLight ${selectedLight === "thirdLight" ? "sombraVerde" : ""}`} onClick={()=> setSelectedLight("thirdLight")}></div>
             </div>
         </div>
 
